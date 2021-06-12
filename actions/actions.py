22efbd2d6204da12e86ee3ab3d7276f3445b19c2
Golchini <a.golchini@outlook.de>
2021-06-12 12:44:08 +0200

    database base

--- actions/actions.py
+++ actions/actions.py
@@ -12,16 +12,25 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-
-class ActionHelloWorld(Action):
+class ActionCheck(Action):
 
     def name(self) -> Text:
-        return "action_hello_world"
+        return "action_check"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        for dbname in tracker.latest_message['entities']:
+            print(tracker.latest_message)
+            if dbname['entity'] == 'database_name':
+                name = dbname['value'].lower()
+                if name == 'sql':
+                    dispatcher.utter_message(text=f"Yes, {name} is a SQL")
 
-        dispatcher.utter_message(text="Hello World!")
+                elif name == 'nosql':
+                    dispatcher.utter_message(text=f"OK, {name} is a NoSQL")
 
+                else:
+                    dispatcher.utter_message(
+                        text=f"I do not recognize {name}, are you sure it is correctly spelled?")
         return []
